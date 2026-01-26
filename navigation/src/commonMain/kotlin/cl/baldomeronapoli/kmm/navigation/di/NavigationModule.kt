2830eb9f428603c7abc9c784a1c0c36afae0ac3d
Baldomero Águila Napoli <baldomero.napoli@gmail.com>
2026-01-26 07:41:10 -0300

    update navigation coordinator

--- navigation/src/commonMain/kotlin/cl/baldomeronapoli/kmm/navigation/di/NavigationModule.kt
+++ navigation/src/commonMain/kotlin/cl/baldomeronapoli/kmm/navigation/di/NavigationModule.kt
@@ -2,9 +2,18 @@ package cl.baldomeronapoli.kmm.navigation.di
 
 import cl.baldomeronapoli.kmm.base.navigation.NavigationCoordinator
 import cl.baldomeronapoli.kmm.navigation.data.repository.NavigationCoordinatorImpl
+import org.koin.core.module.Module
 import org.koin.dsl.module
 
+object NavigationModule {
 
-val navigationModule = module {
-    single<NavigationCoordinator> { NavigationCoordinatorImpl() }
+    fun getModules(): List<Module> {
+        return listOf(
+            commonModule()
+        )
+    }
+
+    private fun commonModule() = module {
+        single<NavigationCoordinator> { NavigationCoordinatorImpl() }
+    }
 }
